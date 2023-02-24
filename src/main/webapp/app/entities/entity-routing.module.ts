28d1c4099fe59107ecffa739214fb91de51f7b87
Igor Fabiano Nazar <ifnazar@gmail.com>
2023-02-24 10:03:46 -0300

    WIP 1

--- src/main/webapp/app/entities/entity-routing.module.ts
+++ src/main/webapp/app/entities/entity-routing.module.ts
@@ -4,6 +4,46 @@ import { RouterModule } from '@angular/router';
 @NgModule({
   imports: [
     RouterModule.forChild([
+      {
+        path: 'region',
+        data: { pageTitle: 'mydatabaseApp.region.home.title' },
+        loadChildren: () => import('./region/region.module').then(m => m.RegionModule),
+      },
+      {
+        path: 'country',
+        data: { pageTitle: 'mydatabaseApp.country.home.title' },
+        loadChildren: () => import('./country/country.module').then(m => m.CountryModule),
+      },
+      {
+        path: 'job',
+        data: { pageTitle: 'mydatabaseApp.job.home.title' },
+        loadChildren: () => import('./job/job.module').then(m => m.JobModule),
+      },
+      {
+        path: 'task',
+        data: { pageTitle: 'mydatabaseApp.task.home.title' },
+        loadChildren: () => import('./task/task.module').then(m => m.TaskModule),
+      },
+      {
+        path: 'employee',
+        data: { pageTitle: 'mydatabaseApp.employee.home.title' },
+        loadChildren: () => import('./employee/employee.module').then(m => m.EmployeeModule),
+      },
+      {
+        path: 'department',
+        data: { pageTitle: 'mydatabaseApp.department.home.title' },
+        loadChildren: () => import('./department/department.module').then(m => m.DepartmentModule),
+      },
+      {
+        path: 'job-history',
+        data: { pageTitle: 'mydatabaseApp.jobHistory.home.title' },
+        loadChildren: () => import('./job-history/job-history.module').then(m => m.JobHistoryModule),
+      },
+      {
+        path: 'location',
+        data: { pageTitle: 'mydatabaseApp.location.home.title' },
+        loadChildren: () => import('./location/location.module').then(m => m.LocationModule),
+      },
       /* jhipster-needle-add-entity-route - JHipster will add entity modules routes here */
     ]),
   ],
---
+++ src/main/webapp/app/entities/job-history/job-history.test-samples.ts
@@ -0,0 +1,30 @@
+import dayjs from 'dayjs/esm';
+
+import { Language } from 'app/entities/enumerations/language.model';
+
+import { IJobHistory, NewJobHistory } from './job-history.model';
+
+export const sampleWithRequiredData: IJobHistory = {
+  id: 95113,
+};
+
+export const sampleWithPartialData: IJobHistory = {
+  id: 80171,
+  endDate: dayjs('2023-02-23T13:37'),
+};
+
+export const sampleWithFullData: IJobHistory = {
+  id: 71556,
+  startDate: dayjs('2023-02-24T11:10'),
+  endDate: dayjs('2023-02-23T19:00'),
+  language: Language['SPANISH'],
+};
+
+export const sampleWithNewData: NewJobHistory = {
+  id: null,
+};
+
+Object.freeze(sampleWithNewData);
+Object.freeze(sampleWithRequiredData);
+Object.freeze(sampleWithPartialData);
+Object.freeze(sampleWithFullData);
